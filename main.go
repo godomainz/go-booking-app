b2a2f6d0e96c4932966b2a53a162cbe63839ba03
Akila Mendis <godomainz@gmail.com>
2025-01-26 22:31:25 +0530

    welcome message

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World")
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("get your tickets here to attend")
 }
 
