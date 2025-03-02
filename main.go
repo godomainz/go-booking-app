8e5b428d5241e20036f4b4861a6c56b12c4a9685
Akila Mendis <godomainz@gmail.com>
2025-03-02 18:05:13 +0530

    Book Ticket Logic

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main()  {
 	var conferenceName string= "Go Conference"
 	const conferenceTickets int= 50
-	var remainingTickets int= 50
+	var remainingTickets uint= 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
@@ -14,7 +14,7 @@ func main()  {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Print("Enter your first name : ")
 	fmt.Scan(&firstName)
@@ -28,6 +28,9 @@ func main()  {
 	fmt.Print("Enter number of tickets : ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n",remainingTickets, conferenceName)
 }
 
