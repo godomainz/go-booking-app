3cf7872232a3e843275ace1fe9bce24524c132a5
Akila Mendis <godomainz@gmail.com>
2025-03-02 11:22:59 +0530

    Getting User Input & What is a Pointer?

--- main.go
+++ main.go
@@ -7,17 +7,27 @@ func main()  {
 	const conferenceTickets int= 50
 	var remainingTickets int= 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n",conferenceTickets, remainingTickets, conferenceName)
-
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Print("Enter your first name : ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your last name : ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your email : ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter number of tickets : ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
 
